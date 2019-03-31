ef319ff980e4465e1f71f2415677a7052aeb1d16
Venkata Raju Bankapalli <raju.intrepid@gmail.com>
2019-04-01 01:51:44 +0530

    Update CustomerClusteringBasedOnAnnualIncomeAndSpendScore.py

--- CustomerClusteringBasedOnAnnualIncomeAndSpendScore.py
+++ CustomerClusteringBasedOnAnnualIncomeAndSpendScore.py
@@ -6,12 +6,62 @@ Created on Sun Nov 11 10:42:32 2018
 """
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
+import os
 import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+#This function is used to load CSV file from the 'data' directory 
+#in the present working directly 
+def loadCSV (fileName):
+    scriptDirectory = os.path.dirname(__file__)
+    dataDirectoryPath = "."
+    dataDirectory = os.path.join(scriptDirectory, dataDirectoryPath)
+    dataFilePath = os.path.join(dataDirectory, fileName)
+    return pd.read_csv(dataFilePath)
+
+#This funtion is used to preview the data in the given dataset
+def previewData (dataSet):
+    print(dataSet.head())
+    print("\n")
+
+#This function is used to check for missing values in a given dataSet
+def checkForMissingValues (dataSet):
+    print(dataSet.isnull().sum())
+    print("\n")
+
+#This function is used to check the statistics of a given dataSet
+def getStatisticsOfData (dataSet):
+    print("***** Datatype of each column in the data set: *****")
+    dataSet.info()
+    print("\n")
+    print("***** Columns in the data set: *****")
+    print(dataSet.columns.values)
+    print("***** Details about the data set: *****")
+    print(dataSet.describe())
+    print("\n")
+    print("***** Checking for any missing values in the data set: *****")
+    checkForMissingValues(dataSet)
+    print("\n")
+    
+#This funtion is used to handle the missing value in the features, in the 
+#given examples
+def handleMissingValues (features):
+    from sklearn.impute import SimpleImputer
+    imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+    imputer.fit(features)
+    imputedFeatures = imputer.fit_transform(features)
+    return imputedFeatures
 
 # Importing the dataset
 dataset = pd.read_csv('mall.csv')
+
+#Preview the dataSet and look at the statistics of the dataSet
+#Check for any missing values 
+#so that we will know whether to handle the missing values or not
+print("***** Preview the dataSet and look at the statistics of the dataSet *****")
+previewData(dataset)
+getStatisticsOfData(dataset)
 
 #Get the features that we want to use while perform the clustering
 #Get the features Annual income and Spending score based
